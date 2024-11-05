1100796173f5bc7cb033e496e8de2f656c7e80de
davebird00 <73djbird@gmail.com>
2024-11-05 15:58:29 -0500

    setting up react and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className="App-header">
+          <header className="landing page headers">
+              <h1>
+                  David Bird
+              </h1>
+              <h2>
+                under construction
+              </h2>
+          </header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
